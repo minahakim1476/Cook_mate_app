474732e7ca59c9a7bacdb38b05fa697563224ca8
Mahmoud Khalil <mahmod.khelo@gmail.com>
2025-11-09 22:07:31 +0200

    save my local changes

--- app/src/main/java/com/example/moviereviewapp/MainActivity.kt
+++ app/src/main/java/com/example/moviereviewapp/MainActivity.kt
@@ -19,29 +19,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MovieReviewAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LoginScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovieReviewAppTheme {
-        Greeting("Android")
-    }
-}
